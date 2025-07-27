1e8e03474699c5e93f7802ddcd37e156c04b75d3
Alexia <106028565+alexia0400@users.noreply.github.com>
2025-07-27 18:23:31 +0300

    Update questions.js

--- questions.js
+++ questions.js
@@ -2,13 +2,13 @@ const questionBank = [
   {
     question: "Quel est le rôle de l’hippocampe ?",
     options: ["Motricité", "Mémoire", "Langage", "Vision"],
-    answer: "Mémoire",
+    answer:1,
     tags: ["neuro", "dfgsm2"]
   },
   {
     question: "Quel nerf crânien est responsable de l’odorat ?",
     options: ["Nerf optique", "Nerf olfactif", "Nerf facial", "Nerf vague"],
-    answer: "Nerf olfactif",
+    answer:1,
     tags: ["neuro", "olfaction"]
   },
   // ... ajoute d'autres questions ici
